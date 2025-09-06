da689902729260727eceaf2ef29460d0ae535d12
islam <lameche1islam@gmail.com>
2025-09-06 18:27:26 +0100

    improved non steam game class constructor parameters

--- NonSteamGame.py
+++ NonSteamGame.py
@@ -7,37 +7,37 @@ class NonSteamGame:
                  name:str,
                  exe:str,
                  dir:str,
-                 icon:Optional[str],
-                 ShortcutPath :Optional[str],
-                 LaunchOptions:Optional[str],
-                 IsHidden : Optional[str],
-                 AllowDesktopConfig : Optional[str],
-                 AllowOverlay : Optional[str],
-                 OpenVR : Optional[str],
-                 Devkit : Optional[str],
-                 DevkitGameID : Optional[str],
-                 DevkitOverrideAppID : Optional[str],
-                 LastPlayTime : Optional[str],
-                 FlatpakAppID : Optional[str],
-                 tags = Optional[str]
+                 icon:str = "",
+                 ShortcutPath:str = "",
+                 LaunchOptions:str = "",
+                 IsHidden:int = 0,
+                 AllowDesktopConfig:int = 1,
+                 AllowOverlay:int = 1,
+                 OpenVR:int = 0,
+                 Devkit:int = 0,
+                 DevkitGameID:str = "",
+                 DevkitOverrideAppID:int = 0,
+                 LastPlayTime:int = 0,
+                 FlatpakAppID:str = "",
+                 tags:dict = {}
                  ):
         self.appid = id
         self.AppName= name
         self.Exe = exe
         self.StartDir = dir
-        self.icon = icon if icon is not None else "" 
-        self.ShortcutPath = ShortcutPath if ShortcutPath is not None else ""
-        self.LaunchOptions = LaunchOptions if LaunchOptions is not None else ""
-        self.IsHidden = IsHidden if IsHidden is not None else 0
-        self.AllowDesktopConfig = AllowDesktopConfig if AllowDesktopConfig is not None else 1
-        self.AllowOverlay = AllowOverlay if AllowOverlay is not None else 1
-        self.OpenVR = OpenVR if OpenVR is not None else 0
-        self.Devkit = Devkit if Devkit is not None else 0
-        self.DevkitGameID = DevkitGameID if DevkitGameID is not None else ""
-        self.DevkitOverrideAppID = DevkitOverrideAppID if DevkitOverrideAppID is not None else 0
-        self.LastPlayTime = LastPlayTime if LastPlayTime is not None else 0
-        self.FlatpakAppID = FlatpakAppID if FlatpakAppID is not None else ""
-        self.tags = tags if tags is not None else {}
+        self.icon = icon 
+        self.ShortcutPath = ShortcutPath 
+        self.LaunchOptions = LaunchOptions 
+        self.IsHidden = IsHidden 
+        self.AllowDesktopConfig = AllowDesktopConfig
+        self.AllowOverlay = AllowOverlay 
+        self.OpenVR = OpenVR 
+        self.Devkit = Devkit 
+        self.DevkitGameID = DevkitGameID 
+        self.DevkitOverrideAppID = DevkitOverrideAppID 
+        self.LastPlayTime = LastPlayTime 
+        self.FlatpakAppID = FlatpakAppID 
+        self.tags = tags 
     
     def as_dict(self):
         return self.__dict__
