733c25c062eef1e74cf52c0ff3cd92b96746f80d
islam <lameche1islam@gmail.com>
2025-09-16 13:19:55 +0100

    Add safe_name function and update SteamDatabase schema

--- src/core/services/steam_db_utils.py
+++ src/core/services/steam_db_utils.py
@@ -1,13 +1,17 @@
 import requests
 import sqlite3
-
+import re
 STEAM_APP_URL = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+
+ILLEGAL_CHARS = r'[<>:"/\\|?*]'
+def safe_name(name: str) -> str:
+    return re.sub(ILLEGAL_CHARS, '', name).strip()
 
 class SteamDatabase:
     def __init__(self, db_path="steam.db"):
         self.conn = sqlite3.connect(db_path)
         self.conn.execute(
-            "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, name TEXT)"
+            "CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, name TEXT, safe_name TEXT UNIQUE)"
         )
 
     def sync(self):
@@ -16,15 +20,16 @@ class SteamDatabase:
         print("Json data fetched successfully")
 
         print("Loading games into local database...")
+
         self.conn.executemany(
-            "INSERT OR IGNORE INTO games (id, name) VALUES (?, ?)",
-            [(app["appid"], app["name"]) for app in apps],
+            "INSERT OR IGNORE INTO games (id, name, safe_name) VALUES (?, ?, ?)",
+            [(app["appid"], app["name"], safe_name(app["name"])) for app in apps],
         )
         self.conn.commit()
         print("Games loaded successfully into db")
-
+    
     def get_steam_id_from_name(self, name: str) -> bool:
-        cur = self.conn.execute("SELECT id FROM games WHERE name = ?", (name,))
+        cur = self.conn.execute("SELECT id FROM games WHERE safe_name = ?", (name,))
         row = cur.fetchone()
         return row[0] if row else None 
     
